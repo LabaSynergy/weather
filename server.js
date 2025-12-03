ed478e30a21533854dc6f0884f47526ccb658015
Denis Doguzhiev <denis.dog@gmail.com>
2025-12-03 21:49:38 +0300

    github pages publication setup

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const axios = require("axios");
-// const path = require("path");
+const path = require("path");
 const app = express();
 const cors = require('cors');
 const PORT = 3000;
 
-// app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(cors());
 
 app.get("/api/weather/:lat/:lng", async (req, res) => {
