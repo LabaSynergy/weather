5f35d05f5dceebed03129c18d478e8a83c68786a
Dek1ms <96451935+Dek1ms@users.noreply.github.com>
2025-12-09 00:42:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080,
-  },
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/weather/', // ← подставьте имя вашего репо
+    server: {
+      port: 8080,
+    },
+  }
 })
